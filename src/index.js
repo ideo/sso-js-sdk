3708b43a2090c35b6bc2b88f804e4e8beef400c6
mtvillwock <mtvillwock@gmail.com>
2020-03-24 13:34:16 -0700

    Replace babel-polyfill with corejs and regenerator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import 'babel-polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import uuidv4 from 'uuid/v4';
 import includes from 'lodash/includes';
 import merge from 'lodash/merge';
